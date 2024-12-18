ecd15168ccf51d1bd944b1d961ba5e94e7839b5b
Guilherme Batista <guilhermebsdeveloper@gmail.com>
2024-12-18 17:54:58 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,18 @@
+// Obtém o botão
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Quando o usuário rolar a página para baixo 300px, o botão aparece
+window.onscroll = function () {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+// Quando o usuário clicar no botão, ele rolará para o topo da página
+scrollToTopBtn.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
 
   
